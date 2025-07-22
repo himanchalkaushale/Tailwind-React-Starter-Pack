c30e36f4dcef4098f4ef796fa0735ebed5976c2b
himanchalkaushale <himanchalkaushale432@gmail.com>
2025-07-22 14:17:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(){
+   return  (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="font-bold text-center">Hello <br />This is tailwind+react starter pack</h1>
+    </div>
+   )
+}
+
+export default App;
